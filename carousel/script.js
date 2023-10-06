6fef2227ee72577d46f914c75d36db11826f2c37
Bilal Khan <bilalkhanrecovered@gmail.com>
2023-10-06 17:30:04 +0500

    created the carousel project

--- carousel/script.js
+++ carousel/script.js
@@ -1,17 +1,63 @@
-const professionals = {
-    firstName: "Susan Smith",
-    firstPersonProfession: "WEB DEVELOPER",
-    secondName: "John Smith",
-    secondPersonProfession: "WEB DEVELOPER",
-    thirdName: "Andrew Smith",
-    thirdPersonProfession: "WEB DESIGNER"
-};
+const reviews = [
+    {
+        id: 1,
+        name: "Susan Smith",
+        job: "Web developer",
+        img: "https://res.cloudinary.com/djywxphqy/image/upload/v1696570177/tj7qhk0jkzdfvpgv1ccg.jpg",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, voluptatem consequuntur ipsum architecto unde eius magnam fuga in. Quasi, ad!",
+    },
+    {
+        id: 2,
+        name: "John Smith",
+        job: "Web designer",
+        img: "https://res.cloudinary.com/djywxphqy/image/upload/v1696570177/lgcouvteh3mz2kjs6pqf.jpg",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, voluptatem consequuntur ipsum architecto unde eius magnam fuga in. Quasi, ad!",
+    },
+    {
+        id: 3,
+        name: "Andrew Smith",
+        job: "Android Developer",
+        img: "https://res.cloudinary.com/djywxphqy/image/upload/v1696570178/vzf0cxd4nkq9jux0al3r.jpg",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, voluptatem consequuntur ipsum architecto unde eius magnam fuga in. Quasi, ad!",
+    },
+]
 
-document.getElementById("first-name").innerHTML = professionals.firstName
-document.getElementById("susan-profession").innerHTML = professionals.firstPersonProfession
+const img = document.getElementById("person-img");
+const author = document.getElementById("author");
+const job = document.getElementById("job");
+const info = document.getElementById("info");
 
-document.getElementById("second-name").innerHTML = professionals.secondName
-document.getElementById("john-profession").innerHTML = professionals.secondPersonProfession
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
 
-document.getElementById("third-name").innerHTML = professionals.thirdName
-document.getElementById("andrew-profession").innerHTML = professionals.thirdPersonProfession
+// set starting item
+let currentItem = 0;
+
+// load initial item
+window.addEventListener("DOMContentLoaded", function() {
+    showPerson(currentItem)
+});
+
+function showPerson(person) {
+    const item = reviews[currentItem];
+    img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.text;
+}
+
+nextBtn.addEventListener("click", function(){
+    currentItem++;
+    if (currentItem > reviews.length - 1) {
+        currentItem = 0;
+    }
+    showPerson(currentItem)
+});
+
+prevBtn.addEventListener("click", function(){
+    currentItem--;
+    if (currentItem < 0) {
+        currentItem = reviews.length -1;
+    }
+    showPerson(currentItem)
+})
