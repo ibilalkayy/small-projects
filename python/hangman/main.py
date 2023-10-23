91670ba3198fe6725ac5446e3e778d10f6de925c
Bilal Khan <bilalkhanrecovered@gmail.com>
2023-10-23 12:28:51 +0500

    created the hangman application

--- python/hangman/main.py
+++ python/hangman/main.py
@@ -9,15 +9,34 @@ def get_random_word():
 
 def hangman():
     word = get_random_word()
-    print(word)
-    print("Guess the word: ")
-    for _ in range(len(word)):
-        print("_", end=" ")
+    word_letters = set(word)
+    guessed_letters = set()
+    attempts = 6
 
-    specific = input("\n\nEnter a letter to guess: ")
-    if specific in word:
-        print("Found")
+    print("Let's play hangman!")
+
+    while len(word_letters) > 0 and attempts > 0:
+        print("You have", attempts, "attempts left. Letters you've used:", ' '.join(guessed_letters))
+
+        current_word = [letter if letter in guessed_letters else '_' for letter in word]
+        print("Current word", ' '.join(current_word))
+
+        specific = input("\nEnter your guess: ").lower()
+        if specific in guessed_letters:
+            print("You've already guessed the letter")
+            continue
+
+        guessed_letters.add(specific)
+
+        if specific in word_letters:
+            word_letters.remove(specific)
+        else:
+            attempts -= 1
+            print("Letter not in the word")
+
+    if attempts == 0:
+        print("Sorry you lost. The word is", word)
     else:
-        print("Not found")
+        print("Congratulations, you guessed the word", word)
 
 hangman()
