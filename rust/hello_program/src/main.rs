cd143e9047f5cdcfd250ca7621f392b0ee382cb9
Bilal Khan <bilalkhanrecovered@gmail.com>
2024-06-30 22:46:50 +0500

    wrote the arry

--- rust/hello_program/src/main.rs
+++ rust/hello_program/src/main.rs
@@ -1,11 +1,10 @@
 fn main() {
-    let mut s1:String = String::from("Hello "); // owner of Hello is s1
-    let length = calculate_length(&mut s1); // transfer ownership to s
-    println!("This length of {} is {}", s1, length);
+    let arr:[&str; 5] = ["Hello", "World", "Bye", "World", "Hi"];
+    write_arr(arr);
+    println!("arr={:?}", arr);
 }
 
-fn calculate_length(s:&mut String) -> usize { // new owner of the Hello
-    s.push_str("World!!");
-    let length:usize = s.len();
-    return length; // return the length of s to s1
+fn write_arr(mut arr1:[&str; 5]) {
+    arr1[0] = "Follow";
+    println!("arr1={:?}", arr1);
 }
