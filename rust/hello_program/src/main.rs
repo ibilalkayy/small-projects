2c7aed5368568cc0a4fa8791ce1f56d2c8f91b86
Bilal Khan <bilalkhanrecovered@gmail.com>
2024-07-01 17:25:14 +0500

    hello program in rust

--- rust/hello_program/src/main.rs
+++ rust/hello_program/src/main.rs
@@ -1,10 +1,4 @@
 fn main() {
-    let arr:[&str; 5] = ["Hello", "World", "Bye", "World", "Hi"];
-    write_arr(arr);
-    println!("arr={:?}", arr);
-}
-
-fn write_arr(mut arr1:[&str; 5]) {
-    arr1[0] = "Follow";
-    println!("arr1={:?}", arr1);
+    println!("Hello world!");
+    println!("I am Rustacean);
 }
