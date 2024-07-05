40701a4bc768ccc48def17036c06028466faafa7
Bilal Khan <bilalkhanrecovered@gmail.com>
2024-07-05 09:41:43 +0500

    updated the rust calculator

--- rust/calculator/src/main.rs
+++ rust/calculator/src/main.rs
@@ -1,36 +1,37 @@
 use std::io;
 
-fn add(num1:i32, num2:i32) {
-    let result:i32 = num1 + num2;
-    println!("Here is the addition result:{}", result);
-}
-
-fn subtract(num1:i32, num2:i32) {
-    let result:i32 = num1 - num2;
-    println!("Here is the subtraction result: {}", result);
-}
-
-fn multiply(num1:i32, num2:i32) {
-    let result:i32 = num1 * num2;
-    println!("Here is the multiplcation result: {}", result);
-}
-
-fn divide(num1:i32, num2:i32) {
-    let result:i32 = num1 / num2;
-    println!("Here is the division result: {}", result);
-}
-
-fn take_input()-> (i32, i32) {
-    let mut input_one = String::new();
-    let mut input_two = String::new();
+fn take_input() -> (i32, i32) {
+    let mut input_one:String = String::new();
+    let mut input_two:String = String::new();
 
     io::stdin().read_line(&mut input_one).expect("failed to read the line");
     io::stdin().read_line(&mut input_two).expect("failed to read the line");
 
+    // convert the string to integer
     let input_one:i32 = input_one.trim().parse().expect("please enter a valid number");
     let input_two:i32 = input_two.trim().parse().expect("please enter a valid number");
 
     return (input_one, input_two);
+}
+
+fn add(num1:i32, num2:i32) {
+    let result:i32 = num1 + num2;
+    println!("This is the addition result: {}", result);
+}
+
+fn subtract(num1:i32, num2:i32) {
+    let result:i32 = num1 - num2;
+    println!("This is the subtraction result: {}", result);
+}
+
+fn multiply(num1:i32, num2:i32) {
+    let result:i32 = num1 * num2;
+    println!("This is the multiplcation result: {}", result);
+}
+
+fn divide(num1:i32, num2:i32) {
+    let result:i32 = num1 / num2;
+    println!("This is the division result: {}", result);
 }
 
 fn main() {
