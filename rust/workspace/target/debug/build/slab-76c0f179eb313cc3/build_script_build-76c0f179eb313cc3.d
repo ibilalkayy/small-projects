9012b7b7558810e4db054c5a9a08112f85351489
Bilal Khan <bilalkhanrecovered@gmail.com>
2024-08-10 18:53:24 +0500

    added the code

--- rust/workspace/target/debug/build/slab-76c0f179eb313cc3/build_script_build-76c0f179eb313cc3.d
+++ rust/workspace/target/debug/build/slab-76c0f179eb313cc3/build_script_build-76c0f179eb313cc3.d
@@ -1,5 +1,5 @@
-/home/ibilalkayy/Documents/Projects/go/src/github.com/ibilalkayy/small-projects/rust/workspace/target/debug/build/slab-76c0f179eb313cc3/build_script_build-76c0f179eb313cc3: /home/ibilalkayy/.cargo/registry/src/index.crates.io-6f17d22bba15001f/slab-0.4.9/build.rs
+/home/ibilalkayy/Documents/Projects/small-projects/rust/workspace/target/debug/build/slab-76c0f179eb313cc3/build_script_build-76c0f179eb313cc3: /home/ibilalkayy/.cargo/registry/src/index.crates.io-6f17d22bba15001f/slab-0.4.9/build.rs
 
-/home/ibilalkayy/Documents/Projects/go/src/github.com/ibilalkayy/small-projects/rust/workspace/target/debug/build/slab-76c0f179eb313cc3/build_script_build-76c0f179eb313cc3.d: /home/ibilalkayy/.cargo/registry/src/index.crates.io-6f17d22bba15001f/slab-0.4.9/build.rs
+/home/ibilalkayy/Documents/Projects/small-projects/rust/workspace/target/debug/build/slab-76c0f179eb313cc3/build_script_build-76c0f179eb313cc3.d: /home/ibilalkayy/.cargo/registry/src/index.crates.io-6f17d22bba15001f/slab-0.4.9/build.rs
 
 /home/ibilalkayy/.cargo/registry/src/index.crates.io-6f17d22bba15001f/slab-0.4.9/build.rs:
--- rust/workspace/target/debug/deps/slab-2ed9ae803522e32b.d
+++ rust/workspace/target/debug/deps/slab-2ed9ae803522e32b.d
@@ -1,6 +1,6 @@
-/home/ibilalkayy/Documents/Projects/go/src/github.com/ibilalkayy/small-projects/rust/workspace/target/debug/deps/libslab-2ed9ae803522e32b.rmeta: /home/ibilalkayy/.cargo/registry/src/index.crates.io-6f17d22bba15001f/slab-0.4.9/src/lib.rs /home/ibilalkayy/.cargo/registry/src/index.crates.io-6f17d22bba15001f/slab-0.4.9/src/builder.rs
+/home/ibilalkayy/Documents/Projects/small-projects/rust/workspace/target/debug/deps/libslab-2ed9ae803522e32b.rmeta: /home/ibilalkayy/.cargo/registry/src/index.crates.io-6f17d22bba15001f/slab-0.4.9/src/lib.rs /home/ibilalkayy/.cargo/registry/src/index.crates.io-6f17d22bba15001f/slab-0.4.9/src/builder.rs
 
-/home/ibilalkayy/Documents/Projects/go/src/github.com/ibilalkayy/small-projects/rust/workspace/target/debug/deps/slab-2ed9ae803522e32b.d: /home/ibilalkayy/.cargo/registry/src/index.crates.io-6f17d22bba15001f/slab-0.4.9/src/lib.rs /home/ibilalkayy/.cargo/registry/src/index.crates.io-6f17d22bba15001f/slab-0.4.9/src/builder.rs
+/home/ibilalkayy/Documents/Projects/small-projects/rust/workspace/target/debug/deps/slab-2ed9ae803522e32b.d: /home/ibilalkayy/.cargo/registry/src/index.crates.io-6f17d22bba15001f/slab-0.4.9/src/lib.rs /home/ibilalkayy/.cargo/registry/src/index.crates.io-6f17d22bba15001f/slab-0.4.9/src/builder.rs
 
 /home/ibilalkayy/.cargo/registry/src/index.crates.io-6f17d22bba15001f/slab-0.4.9/src/lib.rs:
 /home/ibilalkayy/.cargo/registry/src/index.crates.io-6f17d22bba15001f/slab-0.4.9/src/builder.rs:
