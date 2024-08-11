0f3cf89e422976bc7a13b107373aa416cf06279e
Bilal Khan <bilalkhanrecovered@gmail.com>
2024-08-11 19:02:32 +0500

    created a template of the project

--- rust/actix_html/src/main.rs
+++ rust/actix_html/src/main.rs
@@ -1,14 +1,37 @@
 use actix_files::Files;
 use actix_web::{middleware::Logger, App, HttpServer};
 
+fn index() -> impl actix_web::dev::HttpServiceFactory {
+    Files::new("/", "./static/").index_file("index.html")
+}
+
+fn create() -> impl actix_web::dev::HttpServiceFactory {
+    Files::new("/create", "./static/").index_file("create.html")
+}
+
+fn view() -> impl actix_web::dev::HttpServiceFactory {
+    Files::new("/view", "./static/").index_file("view.html")
+}
+
+fn update() -> impl actix_web::dev::HttpServiceFactory {
+    Files::new("/update", "./static/").index_file("update.html")
+}
+
+fn delete() -> impl actix_web::dev::HttpServiceFactory {
+    Files::new("/delete", "./static/").index_file("delete.html")
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    
     println!("Starting the HTTP server at port: 8080");
 
     HttpServer::new(|| {
         App::new()
-            .service(Files::new("/", "./static/").index_file("index.html"))
+            .service(index())
+            .service(create())
+            .service(view())
+            .service(update())
+            .service(delete())
             .wrap(Logger::default())
     })
     .bind(("127.0.0.1", 8080))?
